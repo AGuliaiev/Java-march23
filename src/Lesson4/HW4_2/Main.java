7e684e0a51100a6c0214dd2c62805a765819b109
gulini32 <gulini32@gmail.com>
2023-09-28 13:16:26 +0300

    HW4

--- src/Lesson4/HW4_2/Main.java
+++ src/Lesson4/HW4_2/Main.java
@@ -4,11 +4,45 @@ package Lesson4.HW4_2;
 
 public class Main {
     public static void main(String[] args) {
-        User user = new User(1, "Max", "Petrov", "petr@gmail.com", 35, Gender.MALE, new Car("BMW", 2010, 200));
+        User user1 = new User(1, "Max", "Petrov", "petr@gmail.com", 35, Gender.MALE, new Car("BMW", 2010, 200));
 
-        user.getSkills().add(new Skill("JS", 8));
-        user.getSkills().add(new Skill("JAVA", 9));
-        user.getSkills().add(new Skill("PYTHON", 10));
-        System.out.println(user);
+        user1.getSkills().add(new Skill("JS", 8));
+        user1.getSkills().add(new Skill("JAVA", 9));
+        user1.getSkills().add(new Skill("PYTHON", 10));
+
+        User user2 = new User(1, "Max", "Petrov", "petr@gmail.com", 35, Gender.MALE, new Car("BMW", 2010, 200));
+
+        user2.getSkills().add(new Skill("JS", 8));
+        user2.getSkills().add(new Skill("JAVA", 9));
+        user2.getSkills().add(new Skill("PYTHON", 10));
+
+        User user3 = new User(1, "Max", "Petrov", "petr@gmail.com", 35, Gender.MALE, new Car("BMW", 2010, 200));
+
+        user3.getSkills().add(new Skill("JS", 8));
+        user3.getSkills().add(new Skill("JAVA", 9));
+        user3.getSkills().add(new Skill("PYTHON", 10));
+
+        User user1 = new User(1, "Max", "Petrov", "petr@gmail.com", 35, Gender.MALE, new Car("BMW", 2010, 200));
+
+        user1.getSkills().add(new Skill("JS", 8));
+        user1.getSkills().add(new Skill("JAVA", 9));
+        user1.getSkills().add(new Skill("PYTHON", 10));User user1 = new User(1, "Max", "Petrov", "petr@gmail.com", 35, Gender.MALE, new Car("BMW", 2010, 200));
+
+        user1.getSkills().add(new Skill("JS", 8));
+        user1.getSkills().add(new Skill("JAVA", 9));
+        user1.getSkills().add(new Skill("PYTHON", 10));User user1 = new User(1, "Max", "Petrov", "petr@gmail.com", 35, Gender.MALE, new Car("BMW", 2010, 200));
+
+        user1.getSkills().add(new Skill("JS", 8));
+        user1.getSkills().add(new Skill("JAVA", 9));
+        user1.getSkills().add(new Skill("PYTHON", 10));User user1 = new User(1, "Max", "Petrov", "petr@gmail.com", 35, Gender.MALE, new Car("BMW", 2010, 200));
+
+        user1.getSkills().add(new Skill("JS", 8));
+        user1.getSkills().add(new Skill("JAVA", 9));
+        user1.getSkills().add(new Skill("PYTHON", 10));User user1 = new User(1, "Max", "Petrov", "petr@gmail.com", 35, Gender.MALE, new Car("BMW", 2010, 200));
+
+        user1.getSkills().add(new Skill("JS", 8));
+        user1.getSkills().add(new Skill("JAVA", 9));
+        user1.getSkills().add(new Skill("PYTHON", 10));
+
     }
 }
